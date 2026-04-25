95bca865926d0805fdb3d6467c4f855f95d7cac2
snehalkarad92-creator <snehalkarad92@gmail.com>
2026-04-25 20:02:36 +0530

    github actions - test1

---
+++ calc_test.py
@@ -0,0 +1,15 @@
+import pytest
+from calc import add, sub, mul, div
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_sub():
+    assert add(5, 3) == 2
+
+def test_mul():
+    assert add(2, 3) == 6
+
+def test_div():
+    assert add(10, 2) == 5        
+
