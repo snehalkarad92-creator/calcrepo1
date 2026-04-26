ad107d9bd42cf38f9c302d33a172866b1bc75451
snehalkarad92-creator <snehalkarad92@gmail.com>
2026-04-26 17:01:42 +0530

    added requirements.txt

--- calc_test.py
+++ calc_test.py
@@ -5,11 +5,10 @@ def test_add():
     assert add(2, 3) == 5
 
 def test_sub():
-    assert add(5, 3) == 2
+    assert sub(5, 3) == 2
 
 def test_mul():
-    assert add(2, 3) == 6
+    assert mul(2, 3) == 6
 
 def test_div():
-    assert add(10, 2) == 5        
-
+    assert div(10, 2) == 5
